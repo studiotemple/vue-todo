81bf29497d19ea1838bb1f3306a592e26938b94e
renz81 <58933173+renz81@users.noreply.github.com>
2020-04-29 17:36:45 +0900

    lowdb, lodash setting

---
+++ components/TodoApp.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <todo-item />
+        <todo-creator />
+    </div>
+</template>
+
+<script>
+import lowdb from 'lowdb'
+import LocalStorage from 'lowdb/adapters/LocalStorage'
+import TodoCreator from './TodoCreator'
+import TodoItem from './TodoItem'
+export default {
+    components: {
+        TodoCreator,
+        TodoItem
+    },
+    data () {
+        return {
+            db: null
+        }
+    },
+    created () {
+        this.initDB()
+    },
+    methods: {
+        initDB () {
+            const adapter = new LocalStorage('todo-app')
+            this.db = lowdb(adapter)
+
+            console.log(this.db)
+            //Local DB 초기화
+            this.db.defaults({
+                todos: [] // Collection
+            }).write()
+        }
+    }
+}
+</script>
---
+++ components/TodoItem.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Todo Item</h1>
+</template>
