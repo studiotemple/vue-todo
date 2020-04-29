9d89aeaf39bedc5c20633124fe9e9e7b650483a8
studiotemple <studiotemple81@gmail.com>
2020-04-30 00:23:35 +0900

    add Todo Create component

--- components/TodoApp.vue
+++ components/TodoApp.vue
@@ -1,13 +1,14 @@
 <template>
     <div>
         <todo-item />
-        <todo-creator />
+        <todo-creator @create-todo="createTodo"/>
     </div>
 </template>
 
 <script>
 import lowdb from 'lowdb'
 import LocalStorage from 'lowdb/adapters/LocalStorage'
+import cryptoRandomString from 'crypto-random-string'
 import TodoCreator from './TodoCreator'
 import TodoItem from './TodoItem'
 export default {
@@ -33,6 +34,20 @@ export default {
       this.db.defaults({
         todos: [] // Collection
       }).write()
+    },
+    createTodo (title) {
+      const newTodo = {
+        id: cryptoRandomString({ length: 10 }),
+        title,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        done: false
+      }
+
+      this.db
+        .get('todos') // lodash
+        .push(newTodo) // lodash
+        .write() // lowdb
     }
   }
 }
--- components/TodoCreator.vue
+++ components/TodoCreator.vue
@@ -1,3 +1,35 @@
 <template>
-    <h1>Todo Creator</h1>
+    <div>
+        <button @click="createdTodo">추가</button>
+        <input
+        :value="title"
+        :placeholder="placeholder"
+        type="text"
+        @input="title = $event.target.value"
+        @keypress.enter="createdTodo">
+    </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: '',
+      placeholder: '할 일 추가'
+    }
+  },
+  methods: {
+    createdTodo () {
+      const validatedTitle = this.title && this.title.trim()
+
+      if (!validatedTitle) {
+        alert('입력하신 내용을 확인해주세요')
+        this.title = this.title.trim()
+        return
+      }
+      this.$emit('create-todo', this.title)
+      this.title = ''
+    }
+  }
+}
+</script>
