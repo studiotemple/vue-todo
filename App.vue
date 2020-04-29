ae9957a391b58fdb46a019bc9ddc7e7beba89c82
renz81 <58933173+renz81@users.noreply.github.com>
2020-04-29 16:37:53 +0900

    first commit

---
+++ App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello Vue!'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+$color: blue;
+h1 {
+  color: $color;
+  display: flex;
+}
+</style>
---
+++ main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from './App'
+
+// eslint-disable-next-line
+new Vue({
+  el: '#app',
+  render: h => h(App) // https://css-tricks.com/what-does-the-h-stand-for-in-vues-render-method/
+})
