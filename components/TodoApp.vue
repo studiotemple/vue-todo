20fa4bf65c6ea4e7aff54b99bc37517e9d1769a0
studiotemple <studiotemple81@gmail.com>
2020-05-01 00:24:23 +0900

    update - delete

--- components/TodoApp.vue
+++ components/TodoApp.vue
@@ -1,6 +1,16 @@
 <template>
     <div>
-        <todo-item />
+        <todo-item
+          v-for="todo in todos"
+          :key="todo.id"
+          :todo="todo"
+
+          @update-todo="updateTodo"
+          @delete-todo="deleteTodo"
+        />
+
+        <hr />
+
         <todo-creator @create-todo="createTodo"/>
     </div>
 </template>
@@ -9,8 +19,10 @@
 import lowdb from 'lowdb'
 import LocalStorage from 'lowdb/adapters/LocalStorage'
 import cryptoRandomString from 'crypto-random-string'
+import _cloneDeep from 'lodash/cloneDeep'
 import TodoCreator from './TodoCreator'
 import TodoItem from './TodoItem'
+
 export default {
   components: {
     TodoCreator,
@@ -18,7 +30,8 @@ export default {
   },
   data () {
     return {
-      db: null
+      db: null,
+      todos: []
     }
   },
   created () {
@@ -30,10 +43,20 @@ export default {
       this.db = lowdb(adapter)
 
       console.log(this.db)
-      // Local DB 초기화
-      this.db.defaults({
-        todos: [] // Collection
-      }).write()
+
+      const hasTodos = this.db.has('todos').value()
+
+      if (hasTodos) {
+        // eslint-disable-next-line no-undef
+        this.todos = _cloneDeep(this.db.getState().todos)
+      } else {
+        // Local DB 초기화
+        this.db
+          .defaults({
+            todos: [] // Collection
+          })
+          .write()
+      }
     },
     createTodo (title) {
       const newTodo = {
@@ -48,6 +71,12 @@ export default {
         .get('todos') // lodash
         .push(newTodo) // lodash
         .write() // lowdb
+    },
+    updateTodo () {
+      console.log('updateTodo')
+    },
+    deleteTodo () {
+      console.log('deleteTodo')
     }
   }
 }
--- components/TodoItem.vue
+++ components/TodoItem.vue
@@ -1,3 +1,68 @@
 <template>
-    <h1>Todo Item</h1>
+  <div>
+    <div class="todo-tiem">
+        <input
+            v-model="done"
+            type="checkbox"
+          />
+    </div>
+    <div class="time__title-wrap">
+        <div class="item__title">
+            {{ todo.title }}
+        </div>
+        <div class="item__date">
+            {{ date }}
+        </div>
+    </div>
+    <div class="item__actions">
+       <button @click="onEditMode">수정</button>
+       <button @click="deleteTodo">삭제</button>
+    </div>
+  </div>
 </template>
+
+<script>
+import dayjs from 'dayjs'
+
+export default {
+  props: {
+    todo: Object
+  },
+  computed: {
+    done: {
+      get () {
+        return this.todo.done
+      },
+      set (done) {
+        this.updateTodo({
+          done
+        })
+      }
+    },
+    date () {
+      const date = dayjs(this.todo.createdAt)
+      const isSame = date.isSame(this.todo.updatedAt)
+
+      if (isSame) {
+        return date.format('YYYY년 MM월 DD일')
+      } else {
+        return `${date.format('YYYY년 MM월 DD일')} (수정됨)`
+      }
+    }
+  },
+  methods: {
+    onEditMode () {
+
+    },
+    offEditMode () {
+
+    },
+    updateTodo (value) {
+      this.$emit('update-todo', this.todo, value)
+    },
+    deleteTodo () {
+      this.$emit('delete-todo', this.todo)
+    }
+  }
+}
+</script>
