0e5425b164f8c7ab1682ede9d5db2db3055171c9
renz81 <58933173+renz81@users.noreply.github.com>
2020-05-15 13:48:57 +0900

    test

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ import App from './App'
 // eslint-disable-next-line
 new Vue({
   el: '#app',
-  render: h => h(App) // https://css-tricks.com/what-does-the-h-stand-for-in-vues-render-method/
+  render: h => h(App)
 })
